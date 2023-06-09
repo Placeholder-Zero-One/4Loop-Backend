32adef415118ce72cbaf9d35d164af5330206781
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-06-09 13:42:08 -0500

    Server is setup

--- server.js
+++ server.js
@@ -14,16 +14,23 @@ app.use(express.json()); // Parse JSON request bodies
 const PORT = process.env.PORT || 3001; // Set the port for the server
 
 // Middleware for JWT verification
-const verifyJWT = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `${process.env.JWKS_URI}` // Retrieve JWKS URI from environment variables
-    }),
-    audience: `${process.env.AUDIENCE}`, // Set the JWT audience from environment variables
-    issuer: `${process.env.ISSUER}`, // Set the JWT issuer from environment variables
-    algorithms: ['RS256'] // Set the allowed JWT signing algorithms
-}).unless({ path: ['/books'] }); // Exclude the '/books' path from JWT verification
+// const verifyJWT = jwt({
+//     secret: jwks.expressJwtSecret({
+//         cache: true,
+//         rateLimit: true,
+//         jwksRequestsPerMinute: 5,
+//         jwksUri: `${process.env.JWKS_URI}` // Retrieve JWKS URI from environment variables
+//     }),
+//     audience: `${process.env.AUDIENCE}`, // Set the JWT audience from environment variables
+//     issuer: `${process.env.ISSUER}`, // Set the JWT issuer from environment variables
+//     algorithms: ['RS256'] // Set the allowed JWT signing algorithms
+// }).unless({ path: ['/books'] }); // Exclude the '/books' path from JWT verification
 
-app.use(verifyJWT); // Apply JWT verification to all routes except '/books'
+// app.use(verifyJWT); // Apply JWT verification to all routes except '/books'
+
+
+app.get('/test', (req, res) => {
+    res.send("hello");
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`)); // Start the server and listen on the specified port
